a0e3c15e3cb9646fc18d79cab39b818f2f3cef5f
Dustin Carlino <dabreegster@gmail.com>
2024-04-26 16:47:08 +0100

    Add Loading and SequentialLegend

---
+++ src/lib/SequentialLegend.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  // A horizontal scale of colors
+
+  export let colorScale: string[];
+  export let limits: any[];
+  export let decimalPlaces = 0;
+</script>
+
+<div style="display: flex">
+  {#each colorScale as color}
+    <span style="background: {color}; width: 100%; border: 1px solid black;">
+      &nbsp;
+    </span>
+  {/each}
+</div>
+<div style="display: flex; justify-content: space-between;">
+  {#each limits as limit}
+    {#if decimalPlaces > 0}
+      <span>{limit.toFixed(decimalPlaces)}</span>
+    {:else}
+      <span>{limit}</span>
+    {/if}
+  {/each}
+</div>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,4 @@
 export { default as Legend } from "./Legend.svelte";
+export { default as Loading } from "./Loading.svelte";
 export { default as PropertiesTable } from "./PropertiesTable.svelte";
+export { default as SequentialLegend } from "./SequentialLegend.svelte";

cacc7e9d5e760e70b2e0827a7f189f5e91b2f0ca
Dustin Carlino <dabreegster@gmail.com>
2024-05-31 10:57:29 +0100

    Add a layout with top bar, sidebar, and map

---
+++ src/lib/top_bar_layout/Layout.svelte
@@ -0,0 +1,31 @@
+<div class="top">
+  <slot name="top" />
+</div>
+<div class="left">
+  <slot name="left" />
+</div>
+<div class="main">
+  <slot name="main" />
+</div>
+
+<style>
+  div {
+    float: left;
+  }
+
+  .top {
+    width: 100%;
+    height: 10vh;
+  }
+
+  .left {
+    width: 25%;
+    height: 90vh;
+    overflow: scroll;
+    padding: 8px;
+  }
+  .main {
+    width: 75%;
+    height: 90vh;
+  }
+</style>
---
+++ src/lib/top_bar_layout/index.ts
@@ -0,0 +1,8 @@
+import { writable, type Writable } from "svelte/store";
+
+export { default as Layout } from "./Layout.svelte";
+export { default as SplitComponent } from "./SplitComponent.svelte";
+
+export let topContents: Writable<HTMLDivElement | null> = writable(null);
+export let sidebarContents: Writable<HTMLDivElement | null> = writable(null);
+export let mapContents: Writable<HTMLDivElement | null> = writable(null);

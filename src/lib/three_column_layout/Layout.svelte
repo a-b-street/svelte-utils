6454d7c3bbe712857da2d7448eafaeeaae39f335
Dustin Carlino <dabreegster@gmail.com>
2024-10-25 12:09:48 +0100

    Add a 3-column layout

---
+++ src/lib/three_column_layout/Layout.svelte
@@ -0,0 +1,31 @@
+<div class="left">
+  <slot name="left" />
+</div>
+<div class="main">
+  <slot name="main" />
+</div>
+<div class="right">
+  <slot name="right" />
+</div>
+
+<style>
+  div {
+    float: left;
+  }
+
+  .left {
+    width: 25%;
+    height: 100vh;
+    overflow: scroll;
+    padding: 8px;
+  }
+  .main {
+    width: 50%;
+  }
+  .right {
+    width: 25%;
+    height: 100vh;
+    overflow: scroll;
+    padding: 8px;
+  }
+</style>

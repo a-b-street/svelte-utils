8d3224e58e14084531fa26f570651d1d6c8c5d09
Dustin Carlino <dabreegster@gmail.com>
2024-12-13 16:20:51 +0000

    Set better geocoder options

--- src/lib/map/Geocoder.svelte
+++ src/lib/map/Geocoder.svelte
@@ -1,22 +1,48 @@
 <script lang="ts">
   import { createMapLibreGlMapController } from "@maptiler/geocoding-control/maplibregl";
+  import type { MapController } from "@maptiler/geocoding-control/types";
   import GeocodingControl from "@maptiler/geocoding-control/svelte/GeocodingControl.svelte";
   import maplibregl, { type Map } from "maplibre-gl";
 
   export let apiKey: string;
   export let map: Map | null;
 
-  $: mapController = map
-    ? createMapLibreGlMapController(map, maplibregl)
-    : null;
+  $: mapController = setup(map);
 
-  // TODO Show markers
-  // TODO Set the flyTo duration
+  function setup(map: Map | null): MapController | null {
+    if (!map) {
+      return null;
+    }
+    let marker = true;
+    let showResultMarkers = true;
+    let flyToOptions = {
+      duration: 1000,
+    };
+    let fitBoundsOptions = {
+      duration: 1000,
+    };
+    return createMapLibreGlMapController(
+      map,
+      maplibregl,
+      marker,
+      showResultMarkers,
+      flyToOptions,
+      fitBoundsOptions,
+    );
+  }
 </script>
 
 {#if mapController}
   <div>
-    <GeocodingControl {mapController} {apiKey} />
+    <GeocodingControl
+      {mapController}
+      {apiKey}
+      proximity={[
+        {
+          type: "map-center",
+        },
+      ]}
+    />
   </div>
 {/if}
 

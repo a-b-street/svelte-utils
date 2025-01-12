ebe8244f143467088040aab01c86a19f13809f19
Dustin Carlino <dabreegster@gmail.com>
2025-01-12 14:53:45 +0000

    Fix the cursed layout problems with the top bar. Its size was fixed, not responsive to contents. Trying to fix it hit subtleties with both maplibre and pico.
    Part of https://github.com/nptscot/npw/issues/48.

--- src/lib/top_bar_layout/Layout.svelte
+++ src/lib/top_bar_layout/Layout.svelte
@@ -1,31 +1,44 @@
-<div class="top">
-  <slot name="top" />
-</div>
-<div class="left">
-  <slot name="left" />
-</div>
-<div class="main">
-  <slot name="main" />
+<div class="flex-container">
+  <div class="top">
+    <slot name="top" />
+  </div>
+  <div class="content">
+    <div class="left">
+      <slot name="left" />
+    </div>
+    <div class="main">
+      <slot name="main" />
+    </div>
+  </div>
 </div>
 
 <style>
-  div {
-    float: left;
+  /* Note calling this "container" will clash with a pico built-in */
+  .flex-container {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    margin: 0;
   }
 
   .top {
     width: 100%;
-    height: 10vh;
+    padding: 4px;
+  }
+
+  .content {
+    display: flex;
+    flex: 1;
+    overflow: hidden;
   }
 
   .left {
     width: 25%;
-    height: 90vh;
-    overflow: scroll;
-    padding: 8px;
+    overflow-y: auto;
+    padding: 4px;
   }
+
   .main {
     width: 75%;
-    height: 90vh;
   }
 </style>

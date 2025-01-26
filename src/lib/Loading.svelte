e0752832671b30378bab56a3bacb611310327f2d
Dustin Carlino <dabreegster@gmail.com>
2025-01-26 11:52:12 +0000

    Add fetchWithProgress and update Loading to show it

--- src/lib/Loading.svelte
+++ src/lib/Loading.svelte
@@ -2,14 +2,23 @@
   // Block the rest of the page while 'loading' is non-empty
 
   export let loading: string;
+  export let progress: number | null = null;
 </script>
 
 {#if loading}
-  <div>{loading}</div>
+  <div class="cover">
+    {loading}
+
+    {#if progress != null}
+      <div>
+        <progress value={progress} style:width="100%" />
+      </div>
+    {/if}
+  </div>
 {/if}
 
 <style>
-  div {
+  .cover {
     position: fixed;
     top: 0;
     left: 0;
@@ -17,6 +26,7 @@
     height: 100%;
     background: rgba(0, 0, 0, 0.5);
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     z-index: 999;

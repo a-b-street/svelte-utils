ba5134925541146c24640356d9f12f9946b92fd9
Dustin Carlino <dabreegster@gmail.com>
2025-07-24 15:51:59 +0100

    Test some of the components in a demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,43 @@
-<h1>Welcome to your library project</h1>
-<p>
-  Create your package using @sveltejs/package and preview/showcase your work
-  with SvelteKit
-</p>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+  import { GeoJSON, MapLibre, LineLayer } from "svelte-maplibre";
+  import { Layout } from "../lib/two_column_layout/index.js";
+  import {
+    basemapStyles,
+    Basemaps,
+    StandardControls,
+    MapContextMenu,
+  } from "../lib/map/index.js";
+
+  let style = basemapStyles["Maptiler Dataviz"];
+</script>
+
+<Layout>
+  <div slot="left">
+    <h1>svelte-utils demo</h1>
+  </div>
+
+  <div slot="main" style="position:relative; width: 100%; height: 100vh;">
+    <MapLibre
+      {style}
+      on:error={(e) => {
+        // @ts-ignore ErrorEvent isn't exported
+        console.log(e.detail.error);
+      }}
+      let:map
+    >
+      <StandardControls {map} />
+      <MapContextMenu {map} />
+      <Basemaps bind:style choice="Maptiler Dataviz" />
+    </MapLibre>
+  </div>
+</Layout>
+
+<style>
+  :global(body) {
+    margin: 0px;
+  }
+
+  :global(div) {
+    box-sizing: border-box;
+  }
+</style>
